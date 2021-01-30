32bd506c26f1faffeaa250c795e8d99760eaea76
Philipp Landmann <landmann.philipp@gmx.de>
2021-01-30 15:52:54 +0100

    verbesserte Version (Duplikate werden nicht berechnet)

---
+++ unknownNameSortImproved.py
@@ -0,0 +1,66 @@
+from collections import Counter
+
+# Hier die Liste einfügen:
+inputList = []
+
+
+def findMaxNumber(inputList):
+    """
+    Returned die größte Zahl in der Liste.
+
+    Parameter:
+        inputList (list): Die Liste
+    Returned:
+        maxItem (int): Größte Zahl in der Liste
+    """
+    maxItem = inputList[0]
+    for item in inputList:
+        if maxItem < item:
+            maxItem = item
+    return maxItem
+
+
+def main(unsortiert):
+    """
+    Sortiert eine Liste an Integern (klein > groß) und zeigt die Ergebnisse in der Konsole an.
+    Parameter:
+        inputList (list): Die Liste der Integer
+    Returned:
+        None
+    """
+    # Die sortierte Liste initialisieren:
+    sortiert = []
+
+    # Original-Kopie herstellen:
+    unsortiertOriginal = unsortiert.copy()
+    # Duplikate entfernen:
+    unsortiert = list(set(unsortiert))
+    # Liste der entfernten Einträge herstellen:
+    entferntListe = list((Counter(unsortiertOriginal) - Counter(unsortiert)).elements())
+
+    # Die unsortierte Liste ausgeben:
+    startString = " | ".join(map(str, unsortiert))
+    print(f"{len(startString) * '-'}\n{startString}\n{len(startString) * '-'}")
+
+    # Die größte Zahl an die "sortiert"-Liste
+    # anhängen, bis alles sortiert ist:
+    while len(unsortiert) > 0:
+        # Größte unsortierte Zahl finden:
+        maxNumber = findMaxNumber(unsortiert)
+        # Die Zahl an den Anfang der sortierten Liste anhängen:
+        sortiert.insert(0, maxNumber)
+        # Danach diese von der unsortierten Liste entfernen:
+        unsortiert.remove(maxNumber)
+        # Die sortierte Liste in der Konsole ausgeben:
+        print(" | ".join(map(str, sortiert)))
+
+    # Duplikate wieder einfügen:
+    for item in entferntListe:
+        sortiert.insert(sortiert.index(item), item)
+
+    # Wenn fertig, die sortierte Liste ausgeben:
+    resultString = " | ".join(map(str, sortiert))
+    print(f"{len(resultString) * '-'}\n{resultString}\n{len(resultString) * '-'}")
+
+
+main(inputList)
