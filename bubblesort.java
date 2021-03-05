2f7fc792a94be07468889b836fc0ef8c919c9fbe
Philipp Landmann <landmann.philipp@gmx.de>
2021-03-05 19:40:15 +0100

    added java implementation

---
+++ bubblesort.java
@@ -0,0 +1,27 @@
+public static int[] zahlenVertauschen(array, index1, index2){
+	cachedIndex1 = array[index1];
+	array[index1] = array[index2];
+	array[index2] = cachedIndex1;
+	return array;
+}
+public static int[] bubblesort(int [] inputArray){
+    // veraendert auf True setzen, um in die while-Schleife zu gelangen
+    boolean veraendert = true;
+    // While Schleife überprüft so lange, bis nichts mehr verändert wird >> wir sind fertig
+    while (veraendert == true){
+        // Standardwert der Variable auf False
+        veraendert = false
+        // Jeden Eintrag in der Liste überprüfen
+        for (int index = 0; index < len(inputArray) - 1; index++){
+            // wird ausgeführt, wenn wir zwei Beträge vertauschen können
+            if (inputArray[index] > inputArray[index + 1]){
+                // veraendert auf True setzen, um die Schleife weiterzumachen
+                veraendert = true;
+                // In dieser Funktion vertauschen wir die Zahlen der Liste
+                inputArray = zahlenVertauschen(zahlenVertauschen, index, index + 1);
+	    }
+    	}
+
+    }
+    return inputArray;
+}
